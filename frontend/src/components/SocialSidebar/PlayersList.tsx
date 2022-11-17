4242e3b31717dbb9e314cec536482ca5e303f0fe
erica-sammarco <68513658+erica-sammarco@users.noreply.github.com>
2022-11-17 12:09:14 -0500

    Task 5: UI Teleport Button (#2)
    * reset to jay's changes
    
    * functioning buttons - wip overlap
    
    * reduce div size to fix overlap
    
    * update based on jay's event, remove comments
    
    * remove unused button import
    
    * update test
    
    * fix failing tests - thank you oliver :)
    
    * fix conditional rendering
    
    * update naming per review comment

--- frontend/src/components/SocialSidebar/PlayersList.tsx
+++ frontend/src/components/SocialSidebar/PlayersList.tsx
@@ -1,7 +1,11 @@
-import { Box, Heading, ListItem, OrderedList, Tooltip } from '@chakra-ui/react';
-import React from 'react';
+import { Box, Button, Heading, ListItem, OrderedList, Tooltip } from '@chakra-ui/react';
+import Cancel from '@material-ui/icons/Cancel';
+import MyLocation from '@material-ui/icons/MyLocation';
+import React, { useEffect, useState } from 'react';
+import PlayerController from '../../classes/PlayerController';
 import { usePlayers } from '../../classes/TownController';
 import useTownController from '../../hooks/useTownController';
+import { TeleportRequest } from '../../types/CoveyTownSocket';
 import PlayerName from './PlayerName';
 
 /**
@@ -12,11 +16,56 @@ import PlayerName from './PlayerName';
  */
 export default function PlayersInTownList(): JSX.Element {
   const players = usePlayers();
-  const { friendlyName, townID } = useTownController();
-  const sorted = players.concat([]);
-  sorted.sort((p1, p2) =>
+  const townController = useTownController();
+  const { friendlyName, townID, ourPlayer } = townController;
+  const sortedPlayers = players.concat([]);
+  sortedPlayers.sort((p1, p2) =>
     p1.userName.localeCompare(p2.userName, undefined, { numeric: true, sensitivity: 'base' }),
   );
+
+  const [outgoingTeleport, setOutgoingTeleport] = useState<TeleportRequest | undefined>(
+    ourPlayer.outgoingTeleport,
+  );
+
+  useEffect(() => {
+    ourPlayer.addListener('outgoingTeleportChange', setOutgoingTeleport);
+    return () => {
+      ourPlayer.removeListener('outgoingTeleportChange', setOutgoingTeleport);
+    };
+  }, [ourPlayer]);
+
+  const renderButtons = (player: PlayerController) => {
+    if (player.id !== ourPlayer.id) {
+      if (outgoingTeleport && outgoingTeleport.toPlayerId === player.id) {
+        return (
+          <Button
+            onClick={() => {
+              townController.emitTeleportCanceled(player.id);
+            }}
+            leftIcon={<Cancel fontSize='small' />}
+            size='xs'
+            colorScheme={'red'}
+            margin='1.5'>
+            Cancel
+          </Button>
+        );
+      } else {
+        return (
+          <Button
+            onClick={() => {
+              townController.emitTeleportRequest(player.id);
+            }}
+            leftIcon={<MyLocation fontSize='small' />}
+            size='xs'
+            colorScheme={'blue'}
+            margin='1.5'
+            disabled={outgoingTeleport !== undefined}>
+            Teleport
+          </Button>
+        );
+      }
+    }
+  };
 
   return (
     <Box>
@@ -26,9 +75,10 @@ export default function PlayersInTownList(): JSX.Element {
         </Heading>
       </Tooltip>
       <OrderedList>
-        {sorted.map(player => (
+        {sortedPlayers.map(player => (
           <ListItem key={player.id}>
             <PlayerName player={player} />
+            {renderButtons(player)}
           </ListItem>
         ))}
       </OrderedList>
